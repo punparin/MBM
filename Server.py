86f760546a4641fbc707bd815f53e545f035ca7a
Parin Kobboon <punparin@hotmail.com>
2017-04-03 14:12:55 +0700

    added maximumConnectedClient

--- Server.py
+++ Server.py
@@ -3,14 +3,15 @@ from Handler import *
 from UserManager import *
 
 class Server:
-    def __init__(self, port=9999):
+    def __init__(self, port=9999, maximumClient = 10):
         print("Initializing Server...")
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.host = socket.gethostname()
         self.port = port
+        self.maximumClient = maximumClient
         try:
             self.socket.bind((self.host, self.port))
-            self.socket.listen(5)
+            self.socket.listen(self.maximumClient)
             self.userManager = UserManager(self.socket)
             print("The server is ready!")
             self.listen()
@@ -30,7 +31,7 @@ class Server:
             print("The port is not available")
 
 def main():
-    server = Server()
+    Server()
 
 if __name__ == "__main__":
     main()
