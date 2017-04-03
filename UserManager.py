85a6d37acd29fca351b632fe02bd895be0df96da
Parin Kobboon <punparin@hotmail.com>
2017-04-03 14:36:19 +0700

    Revert "fixed a bug when server is offline"
    This reverts commit 46baf969cb35ae8482939556b2ebf91d7fe7d88a.

--- UserManager.py
+++ UserManager.py
@@ -24,7 +24,7 @@ class UserManager:
         try:
             file_object = open(self.userListFileName, 'rb')
         except FileNotFoundError:
-            file_object = open(self.userListFileName, 'ab')
+            open(self.userListFileName, 'ab')
         try:
             while True:
                 obj = pickle.load(file_object)
