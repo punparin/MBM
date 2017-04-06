6061ff8c00c23863fb3c006c77edf3dfd51f15be
iceyokuna <iceyokuna@hotmail.com>
2017-04-06 17:50:49 +0700

    MBM v0.4
    fixed edit user

--- User.py
+++ User.py
@@ -1,10 +1,25 @@
 class User:
     def __init__(self, username, password, email = ""):
+        # system
         self.username = username
         self.password = password
         self.email = email
 
+        #user
+        self.name = ""
+        self.middle_name = ""
+        self.last_name = ""
+
+        self.nickname = ""
+        self.address = ""
+        self.phone_number = ""
+        self.birth_date = ""
+        self.position = ""
+        self.department = ""
+        self.biology = ""
+        self.nationality =""
+
     def __str__(self):
-        return self.username + " " + self.password + " " + self.email
+        return self.username + " " + self.password + " " + self.email + " " + self.nickname + " " + self.address + " " + self.phone_number
 
 
