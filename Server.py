5d98272ec689ec0447a85288a4f1c06d707d8494
Parin Kobboon <punparin@gmail.com>
2017-04-21 16:18:16 +0700

    added changePassword function

--- Server.py
+++ Server.py
@@ -13,15 +13,6 @@ class Server:
         self.passwordFileName = 'serverPW'
         self.logFileName = 'serverLog'
         try:
-            fileObject = open(self.passwordFileName, 'rb')
-            self.password = pickle.load(fileObject)
-            fileObject.close()
-        except FileNotFoundError:
-            fileObject = open(self.passwordFileName, 'wb')
-            self.password = 'admin'
-            pickle.dump(self.password, fileObject)
-            fileObject.close()
-        try:
             self.socket.bind((self.host, self.port))
             self.socket.listen(self.maximumClient)
             self.userManager = UserManager(self.socket)
@@ -30,10 +21,21 @@ class Server:
             self.thread = threading.Thread(target=self.listen, args=[])
             self.thread.setDaemon(True)
             self.thread.start()
+            try:
+                fileObject = open(self.passwordFileName, 'rb')
+                self.password = pickle.load(fileObject)
+                fileObject.close()
+            except FileNotFoundError:
+                fileObject = open(self.passwordFileName, 'wb')
+                self.password = 'admin'
+                pickle.dump(self.password, fileObject)
+                fileObject.close()
+                print("The initial password is \'admin\', Please change the password as soon as possible")
             self.command()
         except OSError:
             print("Server is already working!")
 
+    # Recieving command from user
     def command(self):
         print('Type \'cm\' to show all the command list ...')
         cm = input()
@@ -42,18 +44,36 @@ class Server:
             cms = cm.split()
             if cms[0] == 'cm':
                 print('1) addAdmin [username] : to promote a user to be admin')
+                print('2) changePassword : to change the password')
                 isCommandValid = True
-            if cms[0] == 'addAdmin' and len(cms) == 2:
-                print('Password: ', end = "")
-                pw = input()
+            elif cms[0] == 'addAdmin' and len(cms) == 2:
+                pw = input("Password: ")
                 if pw == self.password:
                     self.userManager.addAdmin(cms[1])
                 else:
                     print('Invalid password')
                 isCommandValid = True
+            elif cms[0] == 'changePassword':
+                self.changePassword()
+                isCommandValid = True
             if not isCommandValid:
                 print('Invalid Command')
             cm = input()
+
+    def changePassword(self):
+        currentPassword = input("Current Password: ")
+        newPassword = input("New Password: ")
+        confirmedNewPassword = input("Re-enter New Password: ")
+        if currentPassword != self.password:
+            print("Incorrect password")
+        elif newPassword != confirmedNewPassword:
+            print("Passwords are not the same")
+        else:
+            self.password = newPassword
+            fileObject = open(self.passwordFileName, 'wb')
+            pickle.dump(self.password, fileObject)
+            fileObject.close()
+            print("Changed password successfully")
 
     # Waiting for connection, then send the connection to Handler
     def listen(self):
