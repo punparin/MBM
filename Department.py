e109762686e964f6fd4a7ccdbf6184bb39723e5b
Parin Kobboon <punparin@hotmail.com>
2017-04-25 23:20:42 +0700

    added getEmployees() and saveEmployees()

--- Department.py
+++ Department.py
@@ -1,3 +1,5 @@
+import pickle
+
 class InvalidArgument(Exception) : pass
 
 class Department:
@@ -6,14 +8,39 @@ class Department:
         if name in self.departments:
             raise InvalidArgument('The name has already been used')
         self.name = name
-        self.employees = []
+        #self.departmentFileName = name + 'Info'
+        self.employeeIDList = []
         self.positionList = []
         self.subdepartment = []
+    '''
+    def getEmployees(self):
+        try:
+            fileObject = open(self.departmentFileName, 'rb')
+        except FileNotFoundError:
+            fileObject = open(self.departmentFileName, 'ab')
+        try:
+            while True:
+                obj = pickle.load(fileObject)
+                self.employeeIDList.append(obj)
+        except EOFError:
+            fileObject.close()
 
+    def saveEmployee(self, employeeID):
+        fileObject = open(self.departmentFileName, 'ab')
+        pickle.dump(employeeID, fileObject)
+        fileObject.close()
+    '''
     def setName(self, name):
         if name in self.departments:
             raise InvalidArgument('The name has already been used')
         self.name = name
 
-    def addEmployee(self, user):
-        self.employees.append(user)
+    def addEmployee(self, userID):
+        self.employeeIDList.append(userID)
+        print(self.employeeIDList)
+
+    def __str__(self):
+        s = self.name
+        for employeeID in self.employeeIDList:
+            s += "\n\t- " + format(employeeID, '05d')
+        return s
