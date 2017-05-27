207cba1443035013706d673e909c2f486b53ee62
Parin Kobboon <punparin@hotmail.com>
2017-05-27 21:53:52 +0700

    Fix deepcopy bug

--- DepartmentManager.py
+++ DepartmentManager.py
@@ -1,5 +1,6 @@
 import pickle
 import io
+import copy
 from Department import *
 
 class DepartmentManager:
@@ -35,7 +36,7 @@ class DepartmentManager:
             print(err)
 
     def getInitialInfo(self):
-        initialInfo = self.departmentList
+        initialInfo = copy.deepcopy(self.departmentList)
         for department in initialInfo:
             if department.positionTree is not None:
                 for pre, fill, node in RenderTree(department.positionTree):
