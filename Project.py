8c72c43a97f6d5aa4f981742d67bcac968f4eca2
Parin Kobboon <punparin@hotmail.com>
2017-05-28 15:34:10 +0700

    Implement Work, Project, and Project Manager

--- Project.py
+++ Project.py
@@ -1,7 +1,34 @@
 from Work import *
 
+class UserAlreadyExists(Exception) : pass
+class InvalidArgument(Exception) : pass
+
 class Project(Work):
-    def __init__(self):
-        super(Project).__init__()
-        self.title = ""
-    #implement
+    def __init__(self, title, info = None):
+        super().__init__()
+        self.title = title
+        self.info = info
+        self.progress = 0
+
+    def changeTitle(self, title):
+        self.title = title
+
+    def addContributor(self, username):
+        if self.findContributor(username):
+            raise UserAlreadyExists()
+        self.contributorList.append(username)
+
+    def findContributor(self, username):
+        return username in self.contributorList
+
+    def removeContributor(self, username):
+        try:
+            self.contributorList.remove(username)
+        except ValueError:
+            raise InvalidArgument(username + " does not exists")
+
+    def update(self, newInfo):
+        self.info = newInfo
+
+    def updateProgress(self, progress):
+        self.progress = progress
